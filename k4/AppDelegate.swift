b9d5937dc29eff9df28a40b54bbfeaaa99a693dc
sk <994158307@qq.com>
2019-05-15 22:19:12 +0800

    Initial Commit

---
+++ k4/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  k4
+//
+//  Created by sk on 2019/5/15.
+//  Copyright © 2019 sk. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ k4/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  k4
+//
+//  Created by sk on 2019/5/15.
+//  Copyright © 2019 sk. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
